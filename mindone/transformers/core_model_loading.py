7ba9eea5974b9b2b3b2eb34252fe1a2c2a6390c9
The-truthh <821372701@qq.com>
2026-04-27 17:22:32 +0800

    add statement to header and modify version information

--- mindone/transformers/core_model_loading.py
+++ mindone/transformers/core_model_loading.py
@@ -1,6 +1,9 @@
 # Copyright 2025 The HuggingFace Inc. team. All rights reserved.
 # Copyright (c) 2025, MindONE contributors.
 #
+# This code is adapted from https://github.com/huggingface/transformers
+# with modifications to run transformers on mindspore.
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
